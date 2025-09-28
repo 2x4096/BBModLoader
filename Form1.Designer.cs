8e5aa783a265cf219a8e1b6dc53657924b574331
Jackson <Jackson@DESKTOP-IGJ3SMH>
2025-09-27 20:44:09 -0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace BBModLoader
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            modBox = new ListBox();
+            browseDirButton = new Button();
+            gameDirLabel = new Label();
+            launchButton = new Button();
+            patchButton = new Button();
+            launchInfoButton = new Button();
+            patchInfoButton = new Button();
+            label1 = new Label();
+            browseGameDialog = new OpenFileDialog();
+            progressBar = new ProgressBar();
+            progressLabel = new Label();
+            SuspendLayout();
+            // 
+            // modBox
+            // 
+            modBox.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            modBox.BorderStyle = BorderStyle.None;
+            modBox.FormattingEnabled = true;
+            modBox.Location = new Point(12, 44);
+            modBox.Name = "modBox";
+            modBox.Size = new Size(280, 384);
+            modBox.TabIndex = 0;
+            // 
+            // browseDirButton
+            // 
+            browseDirButton.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            browseDirButton.BackColor = SystemColors.Window;
+            browseDirButton.FlatStyle = FlatStyle.Flat;
+            browseDirButton.Location = new Point(442, 56);
+            browseDirButton.Name = "browseDirButton";
+            browseDirButton.Size = new Size(192, 46);
+            browseDirButton.TabIndex = 1;
+            browseDirButton.Text = "Browse";
+            browseDirButton.UseVisualStyleBackColor = false;
+            browseDirButton.Click += browseDirButton_Click;
+            // 
+            // gameDirLabel
+            // 
+            gameDirLabel.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            gameDirLabel.AutoSize = true;
+            gameDirLabel.Location = new Point(316, 63);
+            gameDirLabel.Name = "gameDirLabel";
+            gameDirLabel.Size = new Size(120, 32);
+            gameDirLabel.TabIndex = 2;
+            gameDirLabel.Text = "Game File";
+            // 
+            // launchButton
+            // 
+            launchButton.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            launchButton.BackColor = SystemColors.Window;
+            launchButton.FlatStyle = FlatStyle.Flat;
+            launchButton.Location = new Point(316, 305);
+            launchButton.Name = "launchButton";
+            launchButton.Size = new Size(250, 46);
+            launchButton.TabIndex = 3;
+            launchButton.Text = "Launch";
+            launchButton.UseVisualStyleBackColor = false;
+            launchButton.Click += launchButton_Click;
+            // 
+            // patchButton
+            // 
+            patchButton.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            patchButton.BackColor = SystemColors.Window;
+            patchButton.FlatStyle = FlatStyle.Flat;
+            patchButton.Location = new Point(316, 357);
+            patchButton.Name = "patchButton";
+            patchButton.Size = new Size(250, 46);
+            patchButton.TabIndex = 4;
+            patchButton.Text = "Patch";
+            patchButton.UseVisualStyleBackColor = false;
+            patchButton.Click += patchButton_Click;
+            // 
+            // launchInfoButton
+            // 
+            launchInfoButton.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            launchInfoButton.BackColor = SystemColors.Window;
+            launchInfoButton.FlatStyle = FlatStyle.Flat;
+            launchInfoButton.Location = new Point(572, 305);
+            launchInfoButton.Name = "launchInfoButton";
+            launchInfoButton.Size = new Size(62, 46);
+            launchInfoButton.TabIndex = 5;
+            launchInfoButton.Text = "?";
+            launchInfoButton.UseVisualStyleBackColor = false;
+            launchInfoButton.Click += launchInfoButton_Click;
+            // 
+            // patchInfoButton
+            // 
+            patchInfoButton.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            patchInfoButton.BackColor = SystemColors.Window;
+            patchInfoButton.FlatStyle = FlatStyle.Flat;
+            patchInfoButton.Location = new Point(572, 357);
+            patchInfoButton.Name = "patchInfoButton";
+            patchInfoButton.Size = new Size(62, 46);
+            patchInfoButton.TabIndex = 6;
+            patchInfoButton.Text = "?";
+            patchInfoButton.UseVisualStyleBackColor = false;
+            patchInfoButton.Click += patchInfoButton_Click;
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(74, 32);
+            label1.TabIndex = 7;
+            label1.Text = "Mods";
+            // 
+            // browseGameDialog
+            // 
+            browseGameDialog.DefaultExt = "exe";
+            browseGameDialog.Filter = "Executable Files (*.exe)|*.exe";
+            browseGameDialog.Title = "Browse...";
+            // 
+            // progressBar
+            // 
+            progressBar.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            progressBar.BackColor = SystemColors.Control;
+            progressBar.Location = new Point(316, 269);
+            progressBar.Maximum = 3;
+            progressBar.Name = "progressBar";
+            progressBar.Size = new Size(318, 30);
+            progressBar.TabIndex = 8;
+            // 
+            // progressLabel
+            // 
+            progressLabel.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            progressLabel.AutoSize = true;
+            progressLabel.Location = new Point(419, 224);
+            progressLabel.Name = "progressLabel";
+            progressLabel.Size = new Size(110, 32);
+            progressLabel.TabIndex = 9;
+            progressLabel.Text = "Waiting...";
+            progressLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(646, 450);
+            Controls.Add(progressLabel);
+            Controls.Add(progressBar);
+            Controls.Add(label1);
+            Controls.Add(patchInfoButton);
+            Controls.Add(launchInfoButton);
+            Controls.Add(patchButton);
+            Controls.Add(launchButton);
+            Controls.Add(gameDirLabel);
+            Controls.Add(browseDirButton);
+            Controls.Add(modBox);
+            MinimumSize = new Size(672, 521);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "BB Mod Loader";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ListBox modBox;
+        private Button browseDirButton;
+        private Label gameDirLabel;
+        private Button launchButton;
+        private Button patchButton;
+        private Button launchInfoButton;
+        private Button patchInfoButton;
+        private Label label1;
+        private OpenFileDialog browseGameDialog;
+        private ProgressBar progressBar;
+        private Label progressLabel;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,232 @@
+using System.Diagnostics;
+
+namespace BBModLoader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<Tuple<string, string>> mods = new List<Tuple<string, string>>();
+
+        DirectoryInfo modsDirectory = new DirectoryInfo(Path.Combine(Directory.GetCurrentDirectory(), "Mods"));
+        FileInfo? gameExe = null;
+
+        private void launchInfoButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Patches and launches the mod; doesn't affect the original game.", "BBModLoader", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void patchInfoButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Patches the game; affects the original game.", "BBModLoader", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (!modsDirectory.Exists)
+            {
+                modsDirectory.Create();
+            }
+
+            modBox.Items.Clear();
+
+            foreach (DirectoryInfo modDir in modsDirectory.GetDirectories())
+            {
+                mods.Add(Tuple.Create(modDir.Name, modDir.FullName));
+                modBox.Items.Add(modDir.Name);
+            }
+        }
+
+        private bool RecursiveCopy(string sourceDir, string destinationDir, bool overwrite)
+        {
+            DirectoryInfo dir = new DirectoryInfo(sourceDir);
+            if (!dir.Exists)
+            {
+                MessageBox.Show("Failed to copy; the directory doesn't exist!", "BBModLoader", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            DirectoryInfo[] dirs = dir.GetDirectories();
+            Directory.CreateDirectory(destinationDir);
+
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                string tempPath = Path.Combine(destinationDir, file.Name);
+                file.CopyTo(tempPath, overwrite);
+            }
+
+            foreach (DirectoryInfo subDir in dirs)
+            {
+                string tempPath = Path.Combine(destinationDir, subDir.Name);
+                RecursiveCopy(subDir.FullName, tempPath, overwrite);
+            }
+
+            return true;
+        }
+
+        private bool Patch(DirectoryInfo gameDir)
+        {
+            string? selectedMod = (string?)modBox.SelectedItem;
+            if (selectedMod == null)
+            {
+                MessageBox.Show("Failed to patch; no mod is selected!", "BBModLoader", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            DirectoryInfo? modDir = null;
+            foreach (Tuple<string, string> mod in mods)
+            {
+                if (mod.Item1 == selectedMod)
+                {
+                    modDir = new DirectoryInfo(mod.Item2);
+                }
+            }
+
+            if (modDir == null || !modDir.Exists)
+            {
+                MessageBox.Show("Failed to patch; couldn't find selected mod!", "BBModLoader", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            foreach (FileInfo file in modDir.GetFiles())
+            {
+                string newFilePath = Path.Combine(gameDir.FullName, file.Name);
+                file.CopyTo(newFilePath, true);
+            }
+
+            foreach (DirectoryInfo dir in modDir.GetDirectories())
+            {
+                string? dirName = dir.Name;
+                if (dirName == null)
+                    continue;
+
+                RecursiveCopy(dir.FullName, Path.Combine(gameDir.FullName, dirName), true);
+            }
+
+            return true;
+        }
+
+        private void launchButton_Click(object sender, EventArgs e)
+        {
+            progressBar.Maximum = 3;
+
+            progressBar.Value = 0;
+            progressLabel.Text = "Checking...";
+
+            if (gameExe == null || !gameExe.Exists)
+            {
+                MessageBox.Show("Game path is invalid!", "BBModLoader", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                progressBar.Value = 0;
+                progressLabel.Text = "Waiting...";
+                return;
+            }
+
+            progressBar.Value = 1;
+            progressLabel.Text = "Patching...";
+
+            Random random = new Random();
+            string patchedDirName = $"bbmod-{random.Next()}";
+
+            DirectoryInfo patchedDir = new DirectoryInfo(Path.Combine(Path.GetTempPath(), patchedDirName));
+            while (patchedDir.Exists)
+            {
+                patchedDirName = $"bbmod-{random.Next()}";
+                patchedDir = new DirectoryInfo(Path.Combine(Path.GetTempPath(), patchedDirName));
+            }
+
+            patchedDir.Create();
+
+            DirectoryInfo? gameDir = gameExe.Directory;
+            if (gameDir == null || !gameDir.Exists)
+            {
+                MessageBox.Show("Failed to get the game directory!", "BBModLoader", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                progressBar.Value = 0;
+                progressLabel.Text = "Waiting...";
+                return;
+            }
+
+            foreach (FileInfo file in gameDir.GetFiles())
+            {
+                file.CopyTo(Path.Combine(patchedDir.FullName, file.Name), true);
+            }
+
+            foreach (DirectoryInfo directory in gameDir.GetDirectories())
+            {
+                string? dirName = directory.Name;
+                if (dirName == null)
+                    continue;
+
+                RecursiveCopy(directory.FullName, Path.Combine(patchedDir.FullName, dirName), true);
+            }
+
+            if (!Patch(patchedDir))
+            {
+                progressBar.Value = 0;
+                progressLabel.Text = "Waiting...";
+                return;
+            }
+
+            progressBar.Value = 2;
+            progressLabel.Text = "Launching...";
+
+            string exePath = Path.Combine(patchedDir.FullName, gameExe.Name);
+
+            ProcessStartInfo processStartInfo = new ProcessStartInfo();
+            processStartInfo.FileName = exePath;
+
+            Process.Start(processStartInfo);
+
+            progressBar.Value = 3;
+            progressLabel.Text = "Launched!";
+        }
+
+        private void patchButton_Click(object sender, EventArgs e)
+        {
+            progressBar.Maximum = 2;
+
+            progressBar.Value = 0;
+            progressLabel.Text = "Checking...";
+
+            if (gameExe == null || !gameExe.Exists)
+            {
+                MessageBox.Show("Game executable is invalid!", "BBModLoader", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                progressLabel.Text = "Waiting...";
+                return;
+            }
+
+            progressBar.Value = 1;
+            progressLabel.Text = "Patching...";
+
+            DirectoryInfo? gameDir = gameExe.Directory;
+            if (gameDir == null || !gameDir.Exists)
+            {
+                MessageBox.Show("Failed to get the game directory!", "BBModLoader", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                progressLabel.Text = "Waiting...";
+                return;
+            }
+
+            if (!Patch(gameDir))
+            {
+                progressBar.Value = 0;
+                progressLabel.Text = "Waiting...";
+                return;
+            }
+
+            progressBar.Value = 2;
+            progressLabel.Text = "Patched!";
+
+            MessageBox.Show("Patched!", "BBModLoader", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void browseDirButton_Click(object sender, EventArgs e)
+        {
+            if (browseGameDialog.ShowDialog() == DialogResult.OK)
+            {
+                gameExe = new FileInfo(browseGameDialog.FileName);
+            }
+        }
+    }
+}
